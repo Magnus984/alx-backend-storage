c338eef33c6c2743035cff4962d45d2507205d89
Magnus984 <nharnharyhaw984@gmail>
2024-11-19 10:31:21 +0000

    add

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,7 +18,7 @@ class Cache:
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn: Callable) -> Union[str, bytes, int, float]:
+    def get(self, key: str, fn: Callable=None) -> Union[str, bytes, int, float]:
         """Gets data from redis database"""
         value = self._redis.get(key)
         if value is None:
